0d1e9d2decd7cd87d6686ffe63353a82ec057931
ymrsnz <mounikaymrs@gmail.com>
2026-02-09 10:13:43 +1100

    Create TodoForm.js file

---
+++ src/components/TodoForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './TodoForm.css';
+
+function TodoForm({ onAddTodo }) {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim() !== '') {
+      onAddTodo(input);
+      setInput('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new todo..."
+        className="todo-input"
+      />
+      <button type="submit" className="todo-button">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
