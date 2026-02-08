8c154c44471c67983734dd58640f28548b159ae7
ymrsnz <mounikaymrs@gmail.com>
2026-02-09 10:13:56 +1100

    Create App.js with initial Todo app structure

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>My Todo App</h1>
+        <TodoForm onAddTodo={addTodo} />
+        <TodoList
+          todos={todos}
+          onDeleteTodo={deleteTodo}
+          onToggleTodo={toggleTodo}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
